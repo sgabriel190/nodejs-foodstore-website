f1e39cddff2dfdf02a5611b710855188cfde191e
picolo190 <strilciuc_gabriel98@yahoo.ro>
2020-05-31 12:29:16 +0300

    update app.js code

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 6789;
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser')
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+// Create an express instance
+const app = express();
 
-server.listen(port, hostname, () => {
-  console.log(`Serverul rulează la adresa http://${hostname}:${port}/`);
+// Port + host setup
+const host = "127.0.0.1";
+const port = 8888;
+
+// Express instance setup
+app.set("view engine", "ejs");
+app.set("views", "./templates");
+app.use(expressLayouts);
+app.use(express.static("static"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/**
+ * Setup the host + port that the node js app should communicate with users
+ */
+app.listen(port, host, () => console.log(`Serverul rulează la adresa http://${host}:${port}`));
+
+/**
+ * The root path of the site
+ */
+app.get('/', (req, res) => {
+    res.render("index");
 });
