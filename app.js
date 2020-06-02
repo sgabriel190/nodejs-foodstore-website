00547a9ca95140c03fb5763de47f3c2836473a62
picolo190 <strilciuc_gabriel98@yahoo.ro>
2020-06-02 20:06:51 +0300

    update logging and db

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
 const sessionModule = require("express-session");
 const cookieParser = require("cookie-parser");
+const mongoClient = require("mongodb").MongoClient;
+const urlDB = "mongodb://localhost:27017/";
 
 // Create an express instance
 const app = express();
@@ -41,6 +43,15 @@ app.use(sessionModule({
 // Add cookies to the app
 app.use(cookieParser("abc"));
 
+// Set up the database
+mongoClient.connect(urlDB, { useUnifiedTopology: true }, (err, db) => {
+    if (err) {
+        throw err;
+    }
+    console.log("[DB-info]:Baza de date a fost creata.");
+    db.close();
+});
+
 
 /**
  *********** Web site functionalities ***********
@@ -50,22 +61,38 @@ app.use(cookieParser("abc"));
  * Setup the host + port that the node js app should communicate with users
  */
 app.listen(port, host, () => {
-    console.log(`[Server-info]Serverul rulează la adresa http://${host}:${port}`)
+    console.log(`[Server-info]:Serverul rulează la adresa http://${host}:${port}.`)
 });
 
 /**
  * The root path of the site
  */
 app.get("/", (req, res) => {
-    res.render("index");
+    // Logging message
+    console.log("[Server-info]:Randarea paginii index.");
+    res.render("index", {
+        utilizator: req.cookies["utilizator"]
+    });
 });
 
 // Cont resource HTTP methods
 app.get("/cont", (req, res) => {
-
+    // Logging message
+    console.log("[Server-info]:Randarea paginii cont.");
     res.render("cont", {
-        mesajEroare: req.cookies["mesajEroare"]
+        mesajEroare: req.cookies["mesajEroare"],
+        utilizator: req.cookies["utilizator"]
     });
+});
+
+app.post("/logout", (req, res) => {
+    res.clearCookie("utilizator");
+    res.clearCookie("parola");
+
+    // Logging message
+    console.log("[Server-info]:Log out efectual. Redirectionare catre cont.");
+
+    res.redirect("/cont");
 });
 
 app.post("/login-check", (req, res) => {
@@ -73,21 +100,36 @@ app.post("/login-check", (req, res) => {
 
     if (raspuns_json.nume_utilizator === "admin" && raspuns_json.parola_utilizator === "admin") {
         res.cookie("utilizator", raspuns_json.nume_utilizator);
-        res.cookie("parola", raspuns_json.parola_utilizator);
         res.clearCookie("mesajEroare");
+
+        // Logging message
+        console.log("[Server-info]:Log in cu succes. Redirectionare catre index.");
+
         res.redirect("/");
     } else {
         res.cookie("mesajEroare", "Log in: datele utilizatorului sunt incorecte.");
+
+        // Logging message
+        console.log("[Server-info]:Log in esuat. Redirectionare catre cont.");
+
         res.redirect("/cont");
     }
 });
 
 // Lista resource HTTP methods
 app.get("/lista", (req, res) => {
-    res.render("lista");
+    // Logging message
+    console.log("[Server-info]:Randarea paginii lista.");
+    res.render("lista", {
+        utilizator: req.cookies["utilizator"]
+    });
 });
 
 // Cos-cumparaturi resource HTTP methods
 app.get("/cos-cumparaturi", (req, res) => {
-    res.render("cos-cumparaturi");
+    // Logging message
+    console.log("[Server-info]:Randarea paginii cos-cumparaturi.");
+    res.render("cos-cumparaturi", {
+        utilizator: req.cookies["utilizator"]
+    });
 });
