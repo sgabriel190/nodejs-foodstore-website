acf61ed5f649be58f590b8b8e06f3c1defef8a47
picolo190 <strilciuc_gabriel98@yahoo.ro>
2020-06-03 10:27:01 +0300

    update site

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+function mouseHoverAuthButton() {
+    document.getElementById("buton_autentificare").style.color = "#003d4d";
+}
+
+function mouseOutAuthButton() {
+    document.getElementById("buton_autentificare").style.color = "white";
+}
